c952b1c3676188387bdca7b5123d17c3a8b32ff1
CagataySert <cagatay.sert5@gmail.com>
2022-10-03 18:05:27 +0300

    unncessary relation was removed

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -4,7 +4,6 @@ module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
       User.hasMany(models.LoanRecord, { foreignKey: "borrowerId" });
-      User.hasMany(models.Book, { foreignKey: "borrowerId" });
     }
   }
   User.init(
