69b2ec6b676553af5e29ccb604e500cdbf0d8ad2
CagataySert <cagatay.sert5@gmail.com>
2022-10-03 19:18:23 +0300

    getUsers

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,5 +1,10 @@
 import { User, LoanRecord, Book } from "../database/models";
 
+const checkUserIfExists = async (userId) => {
+  const user = await User.findByPk(userId);
+  return user;
+};
+
 const createUsers = async (req, res) => {
   try {
     await User.bulkCreate([req.body]);
@@ -9,6 +14,33 @@ const createUsers = async (req, res) => {
     console.log(error);
     res.sendStatus(500);
   }
+};
+
+// TODO: Refactor
+const findUserLoandRecordsAndFormat = async (userId) => {
+  const formattedRecords = {
+    past: [],
+    present: [],
+  };
+
+  const records = await LoanRecord.findAll({ where: { borrowerId: userId } });
+
+  if (records.length === 0) {
+    return formattedRecords;
+  }
+
+  records.forEach((record) => {
+    if (!record.userScore) {
+      formattedRecords.present.push({ name: record.name });
+    } else {
+      formattedRecords.past.push({
+        name: record.name,
+        userScore: record.userScore,
+      });
+    }
+  });
+
+  return formattedRecords;
 };
 
 const getUsers = async (req, res) => {
@@ -23,20 +55,28 @@ const getUsers = async (req, res) => {
       return res.json(result);
     }
 
-    const result = await User.findByPk(userId, {
+    const doesUserExist = await checkUserIfExists(userId);
+
+    if (!doesUserExist)
+      return res.json({
+        message: "Can not find any user related sended userId",
+      });
+
+    const user = await User.findByPk(userId, {
       attributes: ["id", "name"],
     });
 
-    res.json(result);
+    const loanRecords = await findUserLoandRecordsAndFormat(userId);
+
+    res.json({
+      id: user.id,
+      name: user.name,
+      books: loanRecords,
+    });
   } catch (error) {
     console.log(error);
     res.sendStatus(500);
   }
-};
-
-const checkUserIfExists = async (userId) => {
-  const user = await User.findByPk(userId);
-  return user;
 };
 
 const borrowBook = async (req, res) => {
@@ -54,7 +94,11 @@ const borrowBook = async (req, res) => {
 
     if (!book) return res.sendStatus(404);
 
+    if (!book.isAccessible)
+      return res.json({ message: "Book was already borrowed by someone!" });
+
     await LoanRecord.create({ bookId, name: book.name, borrowerId: userId });
+    await Book.update({ isAccessible: false }, { where: { id: bookId } });
 
     res.sendStatus(204);
   } catch (error) {
@@ -63,12 +107,15 @@ const borrowBook = async (req, res) => {
   }
 };
 
-const updateBookAvgScore = async (bookId, score) => {
+const updateBookAvgScoreAndAccessibility = async (bookId, score) => {
   const book = await Book.findByPk(bookId);
 
   const updatedAvgScore = book.score < 0 ? score : (book.score + score) / 2;
 
-  await Book.update({ score: updatedAvgScore }, { where: { id: bookId } });
+  await Book.update(
+    { isAccessible: true, score: updatedAvgScore },
+    { where: { id: bookId } }
+  );
 };
 
 const returnBook = async (req, res) => {
@@ -76,7 +123,14 @@ const returnBook = async (req, res) => {
     const { userId, bookId } = req.params;
     const { score } = req.body;
 
-    await LoanRecord.update(
+    const doesUserExist = await checkUserIfExists(userId);
+
+    if (!doesUserExist)
+      return res.json({
+        message: "Can not find any user related sended userId",
+      });
+
+    const isUpdated = await LoanRecord.update(
       { userScore: score },
       {
         where: {
@@ -86,7 +140,9 @@ const returnBook = async (req, res) => {
       }
     );
 
-    await updateBookAvgScore(bookId, score);
+    if (isUpdated[0] > 0) {
+      await updateBookAvgScoreAndAccessibility(bookId, score);
+    }
 
     res.sendStatus(204);
   } catch (error) {
