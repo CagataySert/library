1ee9f47f3aa74dfc8baae3f902e46033267d639e
CagataySert <cagatay.sert5@gmail.com>
2022-10-03 18:05:38 +0300

    returnBook schema

--- src/middlewares/joi/schemas.js
+++ src/middlewares/joi/schemas.js
@@ -8,6 +8,9 @@ const schemas = {
   createBooks: Joi.object({
     name: Joi.string().required(),
   }),
+  returnBook: Joi.object({
+    score: Joi.number().integer().min(1).max(10),
+  }),
 };
 
 export default schemas;
